eb411a3d6fdf67d9b0a1fa7d6829d8b8e582e53a
Allenljl <1114316513@qq.com>
2018-07-02 15:54:32 +0800

    主页

---
+++ pages/index/index.js
@@ -0,0 +1,123 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+    data: {
+        image: [
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 500,
+        iconArray:[
+            {
+                "iconUrl": '../../image/icon-qiandao.png',
+                "iconText": '签到'
+            },
+            {
+                "iconUrl": '../../image/icon-fujin.png',
+                "iconText": '附近'
+            },
+            {
+                "iconUrl": '../../image/icon-zhanhui.png',
+                "iconText": '游展'
+            },
+            {
+                "iconUrl": '../../image/icon-fuli.png',
+                "iconText": '福利'
+            },
+            {
+                "iconUrl": '../../image/icon-muma.png',
+                "iconText": '玩乐'
+            },
+            {
+                "iconUrl": '../../image/icon-xingxing.png',
+                "iconText": '周边'
+            },
+            {
+                "iconUrl": '../../image/icon-tiyu.png',
+                "iconText": '体育'
+            },
+            {
+                "iconUrl": '../../image/icon-qinzi.png',
+                "iconText": '亲子'
+            }
+        ],
+        movieList: [
+            {
+                "movieTitle": "11月20日话剧《风声》",
+                "movieImage": "../../image/huaju.jpeg"
+            },
+            {
+                "movieTitle": "11月20日话剧《风声》2",
+                "movieImage": "../../image/huaju.jpeg"
+            },
+            {
+                "movieTitle": "11月20日话剧《风声》3",
+                "movieImage": "../../image/huaju.jpeg"
+            },
+        ]
+    },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
