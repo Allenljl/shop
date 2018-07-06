f7e7775993ae653b870a88c33f5f98fbe80415c6
Allenljl <1114316513@qq.com>
2018-07-06 09:31:47 +0800

    支付、购物车的数量价格逻辑运算

--- pages/car/car.js
+++ pages/car/car.js
@@ -37,10 +37,19 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
         var getCardata = wx.getStorageSync("GoodsCarList");
         console.log(getCardata);
+
+        //总价格计算
+        var totalData = 0;
+        for (var i = 0; i < getCardata.length; i++) {
+            totalData = totalData + getCardata[i].carPrice * getCardata[i].carNum;
+        }
+
+
         this.setData({
+            totalPrice: totalData,
             carArray: getCardata
         })
     },
@@ -49,7 +58,7 @@ Page({
      * 跳转到支付页面
      */
 
-    getPay: function () {
+    getPay: function() {
         wx.navigateTo({
             url: '../pay/pay',
         })
@@ -57,49 +66,126 @@ Page({
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
+    },
+
+    /**
+     * 生命周期函数--数量加减
+     */
+    carReduce: function(options) {
+        var that = this;
+        var index = options.target.dataset.alphaBeta;
+        var Num = that.data.carArray[index].carNum;
+        var key = "carArray[" + index + "].carNum";
+
+        //修改存储器参数
+        var getCardata = wx.getStorageSync("GoodsCarList");
+
+        var obj = {};
+        if (Num <= 1) {
+            obj[key] = 1;
+            getCardata[index].carNum = 1;
+        } else {
+            Num--;
+            obj[key] = Num;
+            getCardata[index].carNum = Num;
+        }
+
+        //总价格重新计算
+        var totalData = 0;
+        for (var i = 0; i < getCardata.length; i++) {
+            totalData = totalData + getCardata[i].carPrice * getCardata[i].carNum;
+        }
+        obj["totalPrice"] = totalData
+
+        wx.setStorageSync("GoodsCarList", getCardata);
+
+
+        that.setData(obj);
+    },
+
+
+    carAdd: function(options) {
+        var that = this;
+        var index = options.target.dataset.alphaBeta;
+        var Num = that.data.carArray[index].carNum + 1;
+        var key = "carArray[" + index + "].carNum";
+        var obj = {};
+        obj[key] = Num;
+
+        //修改存储器参数
+        var getCardata = wx.getStorageSync("GoodsCarList");
+        getCardata[index].carNum = Num;
+        wx.setStorageSync("GoodsCarList", getCardata);
+
+        //总价格重新计算
+        var totalData = 0;
+        for (var i = 0; i < getCardata.length; i++) {
+            totalData = totalData + getCardata[i].carPrice * getCardata[i].carNum;
+        }
+        obj["totalPrice"] = totalData
+
+
+        that.setData(obj);
+    },
+    /**
+     * 删除购物车商品
+     */
+    carRemove: function(options) {
+        var that = this;
+        var index = options.target.dataset.alphaBeta;
+        var key = "carArray[" + index + "].carShow";
+        var obj = {};
+        obj[key] = false;
+
+        //修改存储器参数
+        var getCardata = wx.getStorageSync("GoodsCarList");
+        getCardata[index].carShow = false;
+        wx.setStorageSync("GoodsCarList", getCardata);
+
+        that.setData(obj);
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
